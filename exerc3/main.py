67523c58ef2329c24b9428b7fa0e781edf4a77fb
Matheus Prudente <matheusprutrevigno292@gmail.com>
2024-08-12 21:09:03 -0300

    exercicios
    exercicio do 1 ao 6

---
+++ exerc3/main.py
@@ -0,0 +1,7 @@
+n1 = int(input("digite o 1º numero:"))
+n2 = int(input("digite o 2º numero:"))
+n3 = int(input("digite o 3º numero:"))
+resultado = n1+n2+n3
+print(f"a soma dos numeros são:{resultado}")
+
+
---
+++ exerc4/main.py
@@ -0,0 +1,6 @@
+n1 = int(input("digite um numero:"))
+n2 = int(input("digite o segundo numero:"))
+if (n1>=n2):
+   print("O primeiro número é maior que o segundo")
+else:
+    print("O segundo número é maior que o primeiro")
